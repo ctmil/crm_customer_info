519ad67418bc5fbf3550f3786d9714de04286235
morrillo <morrillo@umich.edu>
2020-02-03 12:11:53 +0000

    Se arreglaron bugs y se agregaron campos persistidos

--- account.py
+++ account.py
@@ -25,26 +25,41 @@ class account_invoice(models.Model):
 class res_partner(models.Model):
 	_inherit = "res.partner"
 
+	stored_first_unpaid_invoice = fields.Date('Fecha Factura',help='Fecha de la primera factura impaga')
+	stored_last_sale_date = fields.Datetime('Fecha Ultimo Pedido',help='Fecha del ultimo pedido del cliente')
+	stored_avg_sales_amount = fields.Float('Monto Promedio',help='Monto Promedio de los Pedidos')
+	stored_sales_frequency = fields.Integer('Frecuencia de Ventas',help='Frecuencia (en dias) en los cuales el cliente realiza su pedido')
 	first_unpaid_invoice = fields.Date('Fecha Factura',help='Fecha de la primera factura impaga',compute='compute_first_unpaid_invoice')
-	last_sale_date = fields.Date('Fecha Ultimo Pedido',help='Fecha del ultimo pedido del cliente',compute='compute_last_sale_order')
+	last_sale_date = fields.Datetime('Fecha Ultimo Pedido',help='Fecha del ultimo pedido del cliente',compute='compute_last_sale_order')
 	avg_sales_amount = fields.Float('Monto Promedio',help='Monto Promedio de los Pedidos',compute='compute_avg_sales_amount')
 	sales_frequency = fields.Integer('Frecuencia de Ventas',help='Frecuencia (en dias) en los cuales el cliente realiza su pedido',compute='compute_sales_frequency')
 
+
+
+	@api.model
+	def store_crm_info(self):
+            partners = self.env['res.partner'].search([('customer','=',True)])
+            for rec in partners:
+                rec.stored_first_unpaid_invoice = rec.first_unpaid_invoice
+                rec.stored_last_sale_date = rec.last_sale_date
+                rec.stored_avg_sales_amount = rec.avg_sales_amount
+                rec.stored_sales_frequency = rec.sales_frequency
+
+
 	@api.one
 	def compute_first_unpaid_invoice(self):
-		first_invoice = self.env['account.invoice'].search([('state','=','open'),('partner_id','=',self.id)],\
-			order='id asc',limit=1)
+		first_invoice = self.env['account.invoice'].search([('state','=','open'),('partner_id','=',self.id)],order='id asc',limit=1)
 		if first_invoice:
-			self.first_unpaid_invoice = first_invoice.date_invoice
+                    self.first_unpaid_invoice = first_invoice.date_invoice
 		else:
-        		self.first_unpaid_invoice = None
+                    self.first_unpaid_invoice = None
 
 	@api.one
 	def compute_last_sale_order(self):
 		last_sale_order = self.env['sale.order'].search([('state','not in',['draft','sent','cancel']),('partner_id','=',self.id)],\
 			order='id desc',limit=1)
 		if last_sale_order:
-			self.last_sale_date = last_sale_order.date_order
+                    self.last_sale_date = last_sale_order.date_order
 		else:
 			self.last_sale_date = None
 
@@ -71,8 +86,10 @@ class res_partner(models.Model):
 			if not last_date_order:
 				last_date_order = sale_order.date_order
 			else:
-				date_object = datetime.strptime(sale_order.date_order,'%Y-%m-%d %H:%M:%S')
-				last_date_object = datetime.strptime(last_date_order,'%Y-%m-%d %H:%M:%S')
+				#date_object = datetime.strptime(sale_order.date_order,'%Y-%m-%d %H:%M:%S')
+				#last_date_object = datetime.strptime(last_date_order,'%Y-%m-%d %H:%M:%S')
+				date_object = sale_order.date_order
+				last_date_object = last_date_order
 				last_date_order = sale_order.date_order
 				days = days + (date_object - last_date_object).days	
 			index += 1
